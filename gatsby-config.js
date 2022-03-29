a43fa500684fc19fdc836aad7d7219241bba89ed
Bertrand Sauviat <sauviat@adobe.com>
2022-03-29 16:17:19 +0200

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,11 @@ module.exports = {
           title: 'Suppression API',
           description: '',
           path: '/references/suppression.md'
+        },
+        {
+          title: 'Message Execution API',
+          description: '',
+          path: '/references/messaging.md'
         }]
       }
     ]
