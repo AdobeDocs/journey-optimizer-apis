f31456d77a9718760b2552a6c19828cfc63b23bf
dmitrymatio <dmitrymatio@gmail.com>
2022-09-24 02:21:49 +0000

    Deploying to gh-pages from @ AdobeDocs/journey-optimizer-apis@b0279b0f3bc7826e0c9dca0b28020f142a67c20a 🚀

---
+++ component---src-pages-references-sandbox-md-ab06b38b02433823b137.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkjourney_optimizer_apis=self.webpackChunkjourney_optimizer_apis||[]).push([[299],{5048:function(n,t,e){e.r(t),e.d(t,{_frontmatter:function(){return s},default:function(){return m}});var o=e(7462),r=e(3366),u=(e(5007),e(4983)),i=e(1515),p=["components"],s={},a={_frontmatter:s},c=i.Z;function m(n){var t=n.components,e=(0,r.Z)(n,p);return(0,u.mdx)(c,(0,o.Z)({},a,e,{components:t,mdxType:"MDXLayout"}))}m.isMDXComponent=!0}}]);
+//# sourceMappingURL=component---src-pages-references-sandbox-md-ab06b38b02433823b137.js.map
