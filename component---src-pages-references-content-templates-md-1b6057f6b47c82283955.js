206b8523c56acde6f8afa208c264a8eb631d496b
BertrandSAdobe <56609464+BertrandSAdobe@users.noreply.github.com>
2023-08-28 08:18:26 +0000

    Deploying to gh-pages from @ AdobeDocs/journey-optimizer-apis@da140ad1a14854d3932c8d5b1b1d7aab0b518cb7 🚀

--- component---src-pages-references-content-templates-md-1b6057f6b47c82283955.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkjourney_optimizer_apis=self.webpackChunkjourney_optimizer_apis||[]).push([[367],{14002:function(n,t,e){e.r(t),e.d(t,{_frontmatter:function(){return s},default:function(){return m}});var o=e(87462),r=e(63366),u=(e(15007),e(64983)),i=e(91515),p=["components"],s={},a={_frontmatter:s},c=i.Z;function m(n){var t=n.components,e=(0,r.Z)(n,p);return(0,u.mdx)(c,(0,o.Z)({},a,e,{components:t,mdxType:"MDXLayout"}))}m.isMDXComponent=!0}}]);
-//# sourceMappingURL=component---src-pages-references-content-templates-md-1b6057f6b47c82283955.js.map
